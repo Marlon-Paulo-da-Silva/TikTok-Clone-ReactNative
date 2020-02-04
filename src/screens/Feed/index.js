1c7c65ed3d6fabdb53b4503cf1c9648d9ff26928
Marlon-Paulo-da-Silva <marlon.paulo.silva@outlook.com>
2020-02-03 21:57:20 -0300

    terminando LeftContent

--- src/screens/Feed/index.js
+++ src/screens/Feed/index.js
@@ -4,8 +4,16 @@ import {
   View,
   StyleSheet,
   TouchableOpacity,
-  SafeAreaView
+  SafeAreaView,
+  Image
 } from "react-native";
+
+import TextTicker from "react-native-text-ticker";
+
+import comment from "../../../assets/comment.png";
+import iconMusic from "../../../assets/music.png";
+import whiteHeart from "../../../assets/white-heart.png";
+import redHeart from "../../../assets/red-heart.png";
 
 import { Video } from "expo-av";
 
@@ -29,22 +37,44 @@ function Feed() {
         <Video
           source={{
             uri: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"
+            // uri:
+            // "http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4"
           }}
           rate={1.0}
           volume={1.0}
           isMuted={true}
           resizeMode="cover"
           shouldPlay
+          bounce={false}
           isLooping
           style={styles.video}
         />
         <View style={styles.content}>
           <View style={styles.leftContent}>
-            <Text style={styles.name}>@wsl</Text>
+            <Text style={styles.name}>@marlon.psilva</Text>
 
-            <Text style={styles.description}>Descrição</Text>
+            <Text style={styles.description}>
+              Node.js + ReactJS + React Native
+            </Text>
             <Text style={styles.hashtags}>#TikTok</Text>
-            <Text style={styles.music}>Musicas</Text>
+            <TouchableOpacity>
+              <Text style={styles.translate}>VER TRADUÇÂO</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.componentMusic}>
+              <View style={styles.imageIconMusic}>
+                <Image style={styles.iMusic} source={iconMusic} />
+              </View>
+              <TextTicker
+                style={styles.nameMusic}
+                duration={3000}
+                loop
+                bounce
+                repeatSpacer={50}
+                marqueeDelay={800}
+              >
+                I Don’t Care - Ed Sheeran Part Justin Bieber
+              </TextTicker>
+            </TouchableOpacity>
           </View>
           <View style={styles.rightContent}></View>
         </View>
@@ -80,6 +110,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     fontSize: 20
   },
+
   textRightHeader: {
     color: "white",
     paddingHorizontal: 10,
@@ -90,15 +121,39 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     width: "100%",
     position: "absolute",
-    bottom: 60,
+    bottom: 4 + 0,
     zIndex: 2
   },
-  leftContent: {},
+  leftContent: {
+    width: "100%"
+  },
   rightContent: {},
-  name: { color: "white", marginVertical: 5, fontSize: 19, fontWeight: "bold" },
-  description: { color: "white", marginTop: 5, fontSize: 17 },
+  name: { color: "white", marginVertical: 3, fontSize: 17, fontWeight: "bold" },
+  description: { color: "white", marginTop: 2, fontSize: 17 },
   hashtags: { color: "white", fontWeight: "bold" },
-  music: { color: "white", marginTop: 5 },
+  componentMusic: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10
+  },
+  imageIconMusic: {
+    marginRight: 15
+  },
+  iMusic: {
+    width: 20,
+    height: 20,
+    resizeMode: "contain"
+  },
+  nameMusic: {
+    color: "white",
+    fontSize: 15
+  },
+  translate: {
+    fontWeight: "bold",
+    color: "white",
+    marginVertical: 5
+  },
+
   rightContent: {},
   iconProfile: {},
   iconsAction: {},
